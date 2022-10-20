3145ae034416d8a6d67a823b4df313464b98b12c
Derin Owoade <owoadederin6@gmail.com>
2022-10-20 20:43:45 +0100

    Creating log in function and setting up local storage to hold user data on login

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,46 @@
+import { useState } from "react";
+import {
+  Link,
+  RedirectFunction,
+  HistoryRouterProps,
+  redirect,
+} from "react-router-dom";
 export default function Home() {
-  return <div></div>;
+  const [userName, setUserName] = useState("");
+  return (
+    <div>
+      <div className="bg-gray-100 w-screen h-screen text-4xl flex items-center justify-center">
+        <div className=" flex flex-col items-center justify-center">
+          <div className="mt-2">
+            <input
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            ></input>
+            <button
+              className="border"
+              onClick={() => {
+                if (!userName) return;
+              }}
+            >
+              {userName && (
+                <Link
+                  to={"/chat"}
+                  onClick={() => {
+                    if (!userName) return;
+                    localStorage.setItem(
+                      "currentUser",
+                      JSON.stringify(userName)
+                    );
+                  }}
+                >
+                  Login
+                </Link>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
