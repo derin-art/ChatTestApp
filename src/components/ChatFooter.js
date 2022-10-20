58ae54e6ed7b69791f9bfafe36b6ad7ebb3a54c2
Derin Owoade <owoadederin6@gmail.com>
2022-10-20 20:14:26 +0100

    Setting up initial function to handle send messages

--- src/components/ChatFooter.js
+++ src/components/ChatFooter.js
@@ -1,3 +1,28 @@
-export default function ChatFooter() {
-  return <div></div>;
+import { useState } from "react";
+
+export default function ChatFooter({ socket }) {
+  const [message, setMessage] = useState("");
+  const handleClick = () => {
+    socket.emit("message", {
+      text: message,
+      id: `${socket.id}${Math.random()}`,
+      socketID: socket.id,
+    });
+  };
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        Send
+      </button>
+    </div>
+  );
 }
