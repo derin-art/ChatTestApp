ba0e817e0ab64df062dc2f1a9d7cb3907afdfca9
Derin Owoade <owoadederin6@gmail.com>
2022-10-20 20:09:48 +0100

    Creating ChatFooter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import socketIO from "socket.io-client";
+import Home from "./components/Home";
+import ChatPage from "./components/ChatPage";
 const socket = socketIO.connect("http://localhost:4000");
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home socket={socket}></Home>}></Route>
+        <Route
+          path="/chat"
+          element={<ChatPage socket={socket}></ChatPage>}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
