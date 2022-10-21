882c7fb9e28afd665ab3c2acac1d3be2fc27e703
Derin Owoade <owoadederin6@gmail.com>
2022-10-21 02:32:27 +0100

    Rendering of messages from socketio

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,16 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+import { render, fireEvent, screen } from "@testing-library/react";
+import ChatBody from "./components/ChatBody";
+import ChatFooter from "./components/ChatFooter";
+
+test("rendersMessage", () => {
+  render(<ChatFooter></ChatFooter>);
+  const sendButton = screen.getAllByTestId("sendButton");
+  const messageBox = screen.getAllByTestId("messageBox");
+  fireEvent.click(sendButton);
+  expect(messageBox).toHaveValue("");
+});
